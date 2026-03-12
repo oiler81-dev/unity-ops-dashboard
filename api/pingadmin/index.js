f0652c0d28f348203f1b3669ab1bbdc240264887
oiler81-dev <oiler81@gmail.com>
2026-03-12 19:46:25 -0400

    Update index.js

--- api/pingadmin/index.js
+++ api/pingadmin/index.js
@@ -1,12 +1,8 @@
 module.exports = async function (context, req) {
+  context.log && context.log('pingadmin invoked');
   context.res = {
     status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: {
-      ok: true,
-      message: "pingadmin is live"
-    }
+    headers: { "Content-Type": "application/json" },
+    body: { ok: true }
   };
 };
