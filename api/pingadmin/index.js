a233eab01e8b039ff5a9c570bab4d2796735b116
oiler81-dev <oiler81@gmail.com>
2026-03-16 15:40:31 -0700

    Update index.js

--- api/pingadmin/index.js
+++ api/pingadmin/index.js
@@ -1,8 +1,10 @@
 module.exports = async function (context, req) {
-  context.log && context.log('pingadmin invoked');
   context.res = {
     status: 200,
     headers: { "Content-Type": "application/json" },
-    body: { ok: true }
+    body: {
+      ok: true,
+      timestamp: new Date().toISOString()
+    }
   };
 };
