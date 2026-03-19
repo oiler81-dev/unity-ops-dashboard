e71753f365b9dc92fb240d469e6779d27440b719
oiler81-dev <oiler81@gmail.com>
2026-03-19 14:24:26 -0700

    Update index.js

--- api/import-excel/index.js
+++ api/import-excel/index.js
@@ -1,10 +1,14 @@
 module.exports = async function (context, req) {
   context.res = {
     status: 200,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: {
       ok: true,
-      message: "import-excel route is alive"
+      message: "import-excel route is alive",
+      method: req.method || "",
+      time: new Date().toISOString()
     }
   };
 };
