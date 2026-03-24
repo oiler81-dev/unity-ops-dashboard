d41a2a4b4f1c45ee17e2f3957c49fbc68b0ee362
oiler81-dev <oiler81@gmail.com>
2026-03-24 14:30:04 -0700

    Update table.js

--- api/shared/table.js
+++ api/shared/table.js
@@ -1,1 +1,17 @@
-module.exports = {};
+const { TableClient } = require("@azure/data-tables");
+
+function getConnectionString() {
+  const value = process.env.AZURE_TABLES_CONNECTION_STRING;
+  if (!value) {
+    throw new Error("Missing AZURE_TABLES_CONNECTION_STRING");
+  }
+  return value;
+}
+
+function getTableClient(tableName) {
+  return TableClient.fromConnectionString(getConnectionString(), tableName);
+}
+
+module.exports = {
+  getTableClient
+};
