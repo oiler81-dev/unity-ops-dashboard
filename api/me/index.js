d85dda315cf2dc41fff1b4cd18988f362534e6a9
oiler81-dev <oiler81@gmail.com>
2026-03-31 12:31:38 -0700

    Update index.js

--- api/me/index.js
+++ api/me/index.js
@@ -5,5 +5,9 @@ const { ok } = require("../shared/response");
 module.exports = async function (context, req) {
   const user = getUserFromRequest(req);
   const access = resolveAccess(user);
-  return ok({ user, access });
+
+  return ok({
+    user,
+    access
+  });
 };
